4e96bb29fe2b364643f2f54ebd14b9107d2d1c14
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-19 01:17:23 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,8 +4,12 @@
 def run_guessing_game
 puts "Guess a number between 1 and 6."
 answer = gets.chomp
-  if answer == "exit"
+while comp = rand(1..6)
+  if answer == comp
+    puts "You guessed the correct number!"
+  elsif answer == "exit"
     puts "Goodbye!"
+    break
   end
 
 
